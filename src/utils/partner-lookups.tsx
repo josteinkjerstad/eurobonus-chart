82b6f008ae2b72e2b0fbd47ed0a30867618bc4a1
josteinkjerstad <jostein.kjerstad@sonat.no>
2025-03-17 13:11:47 +0100

    Update partner-lookups.tsx

--- src/utils/partner-lookups.tsx
+++ src/utils/partner-lookups.tsx
@@ -93,7 +93,8 @@ const isNorgesgruppen = (activity: string): boolean => activity.includesAny(["No
 const isRadisson = (activity: string): boolean => activity.includesAny(["Radisson", "Rezidor SAS"]);
 const isEuroBonusEarnShop = (activity: string): boolean => activity.includesAny(["EuroBonus Earn Shop", "EuroBonus Shop (NOK)", "Onlineshopping"]);
 const isPartnerFlight = (activity: string): boolean => activity.includesAny(Object.keys(AirlinePartner).map(key => `| ${key}`));
-export const isRefund = (activity: string): boolean => activity.includesAny(["Refund", "Scandinavian Airlines Spec Actv | Points Corrections"]);
+export const isRefund = (activity: string): boolean =>
+  activity.includesAny(["Refund", "Scandinavian Airlines Spec Actv | Points Corrections", "SAS EuroBonus | Points Corrections"]);
 
 export const findVendor = (transaction: Transaction): Vendor => {
   switch (true) {
