204f6e4f7674abed49c426075aaf3730951c6ee6
josteinkjerstad <jostein.kjerstad@sonat.no>
2025-12-07 00:23:35 +0100

    Update EurobonusDashboard.tsx

--- src/components/EurobonusDashboard.tsx
+++ src/components/EurobonusDashboard.tsx
@@ -2,11 +2,8 @@ import { useMemo } from "react";
 import useFetchProfiles from "../hooks/useFetchProfiles";
 import useFetchTransactions from "../hooks/useFetchTransactions";
 import { EurobonusCharts } from "./charts/eurobonus/EurobonusCharts";
-import type { Transaction } from "../models/transaction";
-import type { Profile } from "../models/profile";
 import { Spinner } from "@blueprintjs/core";
 import { calculateTotalEuroBonusPoints, getEarliestDate, getLatestDate } from "../utils/calculations";
-import { useSummarizedPoints } from "../hooks/useSummarizedPoints";
 import { Tooltip, IconButton } from "@mui/material";
 import InfoOutlined from "@mui/icons-material/InfoOutlined";
 
@@ -14,8 +11,8 @@ export const EurobonusDashboard = () => {
   const { data, loading } = useFetchTransactions();
   const { data: profilesData, loading: profileLoading } = useFetchProfiles();
 
-  const transactions: Transaction[] = useMemo(() => (data ? data : []), [data]);
-  const profiles: Profile[] = useMemo(() => (profilesData ? profilesData : []), [profilesData]);
+  const transactions = useMemo(() => (data ? data : []), [data]);
+  const profiles = useMemo(() => (profilesData ? profilesData : []), [profilesData]);
   const sum = useMemo(() => calculateTotalEuroBonusPoints(transactions), [transactions]);
   const earliestdate = useMemo(() => getEarliestDate(transactions), [transactions]);
   const latestDate = useMemo(() => getLatestDate(transactions), [transactions]);
